ac6b5873a7b34d56b8966b0de67bec41dcbef82b
Gulsen Genc <gulsengenctuncer@gmail>
2022-05-09 23:55:49 +0300

    09.05.2022

---
+++ src/Varargs/ENG_varargs_01.java
@@ -0,0 +1,26 @@
+package Varargs;
+
+public class ENG_varargs_01 {
+    public static void main(String[] args) {
+        /*
+        Write a return method that accepts more
+        than one integer as parameter and prints the sum of integers Method name = sum
+        if you call method like that sum(1,2,3) output =6 sum(1,2,3,4,5) output =15 sum(1,2) output=3
+        -->girilen tam sayıların toplamını return eden bır method ceate edın
+         */
+        int a=1;
+        int b=2;
+        int c=3;
+        int d=4;
+        sumNumbers(1,2,3,4);
+    }
+
+    private static void sumNumbers(int... number) {
+        int sum=0;
+        for (int w:number
+             ) {
+            sum+=w;
+        }
+        System.out.println("sum of numbers="+sum);
+    }
+}
